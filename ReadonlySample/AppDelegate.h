12be6f7c6c8387a4dbaacc0c1a5e9df057c0f4ca
naosim <nao_pillows@yahoo.co.jp>
2012-12-28 06:36:46 +0900

    Initial Commit

---
+++ ReadonlySample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ReadonlySample
+//
+//  Created by 藤田 直 on 12/12/28.
+//  Copyright (c) 2012年 藤田 直. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ReadonlySample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ReadonlySample
+//
+//  Created by 藤田 直 on 12/12/28.
+//  Copyright (c) 2012年 藤田 直. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
